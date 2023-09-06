9864565dc7262f33fee17196c87babae9ef9ee12
Prasan-V <130823121+Prasan-V@users.noreply.github.com>
2023-09-06 12:18:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('Hello, I am Prasan Vibhuti. I am an intern at Digirise Infolabs, I have completed my BCA from Gogte College of Commerce')
+
+
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler)],
+                                         debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
